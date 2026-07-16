a7bc9bc9bbbcebd1a34a668a7a0a652376d8f460
Berkay62 <berkaydogan224561@gmail.com>
2026-07-16 16:56:10 +0300

    validation added version of DailyWordController.java

--- src/main/java/com/berkay/ieltsapp/controller/DailyWordController.java
+++ src/main/java/com/berkay/ieltsapp/controller/DailyWordController.java
@@ -4,32 +4,35 @@ import com.berkay.ieltsapp.service.DailyWordService;
 import com.berkay.ieltsapp.service.UserService;
 import com.berkay.ieltsapp.entity.AppUser;
 import com.berkay.ieltsapp.repository.UserRepo;
+import jakarta.validation.constraints.Positive;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+import jakarta.validation.Valid;
 import java.util.*;
 @RestController
 @RequestMapping("/API/DailyWord")
+@Validated
 public class DailyWordController {
     private final DailyWordService service;
     public DailyWordController(DailyWordService service){
         this.service=service;
     }
     @GetMapping("/{ID}")
-    public List<DailyWord>getTodayWordsForUser(@PathVariable("ID") Long ID){
+    public List<DailyWord>getTodayWordsForUser(@PathVariable("ID") @Positive Long ID){
         return service.getTodayWordsForUser(ID);
     }
 
     @PutMapping("/{ID}/completed")
-    public DailyWord markAsCompleted(@PathVariable("ID") Long Id){
+    public DailyWord markAsCompleted(@PathVariable("ID") @Positive Long Id){
         return service.markAsCompleted(Id);
     }
     @GetMapping("/{ID}/completed")
-    public List<DailyWord> getCompletedWord(@PathVariable("ID") Long Id){
+    public List<DailyWord> getCompletedWord(@PathVariable("ID") @Positive Long Id){
         return service.getCompletedWord(Id);
     }
     @GetMapping("/{ID}/uncompleted") // methodaların endpointleri faklı olmak zorunda
     // eğer endpointler aynı olursa mappingte sıkıntı çıkıyor
-    public List<DailyWord> getUncompletedWord(@PathVariable("ID")Long ID){
+    public List<DailyWord> getUncompletedWord(@PathVariable("ID") @Positive Long ID){
         return service.getUnCompletedWord(ID);
     }
 
