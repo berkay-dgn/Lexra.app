a4afbfbac6e71a3a9e713b2755c13458f64ab43f
Berkay62 <berkaydogan224561@gmail.com>
2026-07-17 17:18:30 +0300

    AiSentenceResult class

---
+++ src/main/java/com/berkay/ieltsapp/dto/AiSentenceResult.java
@@ -0,0 +1,8 @@
+package com.berkay.ieltsapp.dto;
+import com.berkay.ieltsapp.*;
+import java.util.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+public class AiSentenceResult {
+
+}
