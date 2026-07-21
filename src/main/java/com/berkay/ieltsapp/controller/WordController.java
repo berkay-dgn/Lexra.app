0ac84de3c685ce60ea5ee42eb1df060bfdf0a0bf
Berkay62 <berkaydogan224561@gmail.com>
2026-07-21 13:35:39 +0300

    I added validation in WordController.java for avoid any bugs

--- src/main/java/com/berkay/ieltsapp/controller/WordController.java
+++ src/main/java/com/berkay/ieltsapp/controller/WordController.java
@@ -2,12 +2,15 @@ package com.berkay.ieltsapp.controller;
 import com.berkay.ieltsapp.entity.WordList;
 import com.berkay.ieltsapp.service.WordService;
 import com.berkay.ieltsapp.repository.WordRepository;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+import jakarta.validation.Valid;
 import java.util.List;
-
 @RequestMapping("/API/words")
 @RestController
+@Validated
 public class WordController {
     private final WordService wordService;
     public WordController(WordService wordService1){
@@ -18,15 +21,15 @@ public class WordController {
         return wordService.showList();
     }
     @GetMapping("/{id}")
-    public WordList getById( @PathVariable long id){
+    public WordList getById( @PathVariable @Positive long id){
         return wordService.getWordById(id);
     }
     @PostMapping
-    public WordList addWord(@RequestBody WordList word){
+    public WordList addWord(@RequestBody @Valid WordList word){
         return wordService.addWord(word);
     }
     @DeleteMapping("/{id}")
-    public void deleteWord(@PathVariable  long id){
+    public void deleteWord(@PathVariable @Positive long id){
         wordService.deleteWord(id);
     }
 }
