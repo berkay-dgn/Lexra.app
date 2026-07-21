f41e62649387c833237ddf4883c2fc50ca490ab6
Berkay62 <berkaydogan224561@gmail.com>
2026-07-21 13:30:40 +0300

    newest version of files

--- src/main/java/com/berkay/ieltsapp/dto/AiSentenceResult.java
+++
@@ -1,8 +0,0 @@
-package com.berkay.ieltsapp.dto;
-import com.berkay.ieltsapp.*;
-import java.util.*;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.*;
-public class AiSentenceResult {
-
-}
