f6c131c7e69da6300055c06c4b7164744d23f9ab
Berkay62 <berkaydogan224561@gmail.com>
2026-07-21 13:37:15 +0300

    Newest version of WordRepository.java

--- src/main/java/com/berkay/ieltsapp/repository/WordRepository.java
+++ src/main/java/com/berkay/ieltsapp/repository/WordRepository.java
@@ -2,5 +2,4 @@ package com.berkay.ieltsapp.repository;
 import com.berkay.ieltsapp.entity.WordList;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface WordRepository extends JpaRepository<WordList,Long> {
-
 }
