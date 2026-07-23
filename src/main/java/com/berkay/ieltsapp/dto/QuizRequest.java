0af069a8064fbd91052475a12537b129626474c0
Berkay62 <berkaydogan224561@gmail.com>
2026-07-23 16:54:36 +0300

    Level parameter's has been added

--- src/main/java/com/berkay/ieltsapp/dto/QuizRequest.java
+++ src/main/java/com/berkay/ieltsapp/dto/QuizRequest.java
@@ -11,13 +11,20 @@ public class QuizRequest {
     @JsonProperty("userId")
     @Positive
     private Long userId;
-    public QuizRequest(int quizLimit, Long userId){
+    @JsonProperty("Level")
+    @Pattern(regexp = "A1|A2|B1|B2|C1|C2"
+            ,message="Level must be between A1,C2")
+    private String level;
+    public QuizRequest(int quizLimit, Long userId,String level){
         this.quizLimit = quizLimit;
         this.userId=userId;
+        this.level=level;
     }
     public void setUserId(Long userId){this.userId=userId;}
     public void setQuizLimit(int quizLimit){this.quizLimit = quizLimit;}
+    public void setLevel(String level){this.level=level;}
 
     public Long getUserId(){return userId;}
     public int getQuizLimit(){return quizLimit;}
+    public String getLevel(){return level;}
 }
